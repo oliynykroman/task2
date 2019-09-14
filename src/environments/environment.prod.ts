996b819a8e8d8059c92d4ddebc55d15e84a6e2e4
Roman Oliynyk <oliynykroma@live.com>
2019-09-14 21:32:56 +0300

    changed nvironments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   domain:'https://oliynykroman.github.io/task2',
-  apiDomain: 'http://localhost:3000',
+  apiDomain: 'https://oliynykroman.github.io/task2',
 };
