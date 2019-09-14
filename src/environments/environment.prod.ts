4bc159308d50c3970dfc5be3367450c6313560fa
Roman Oliynyk <oliynykroma@live.com>
2019-09-14 22:26:40 +0300

    updated envornments with fake server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   domain:'https://oliynykroman.github.io/task2',
-  apiDomain: 'https://oliynykroman.github.io/task2',
+  apiDomain: 'https://my-json-server.typicode.com/oliynykroman/task2',
 };
